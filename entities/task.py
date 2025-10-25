abc7138f0dc5b559a8fdf7b5b67201dcba4e0e17
Assemian <essoi.assemian@gmail.com>
2025-10-25 12:16:49 +0000

    création de tâches

---
+++ entities/task.py
@@ -0,0 +1,25 @@
+from datetime import datetime
+
+class Task:
+    
+    def __init__(self, title:str, description:str, status:str, create_at:datetime,update:datetime):
+        self.__title = title
+        self.__description = description
+        self.__status = status
+        self.__create_at = create_at
+        self.__update = update 
+
+    @classmethod   
+    def create (cls,title:str , description:str):
+        return Task(
+            title=title,
+            description=description,
+            status="pending",
+            create_at=datetime.now(),
+            update= None)
+    
+    
+    
+    def __repr__(self):
+        return f"(title: {self.__title}, description: {self.__description}, status: {self.__status}, create_at: {self.__create_at}, update: {self.__update})"
+    
---
+++ main.py
@@ -0,0 +1,5 @@
+
+from entities.task import Task
+
+task = Task.create(title="test oto", description= "description")
+print(task)
