3d6b8474374e56ea5c90f194d45cbb7e13cdb1ed
Assemian <essoi.assemian@gmail.com>
2025-10-25 13:15:07 +0000

    commencer une tâche, mettre fin à une tâche

--- entities/task.py
+++ entities/task.py
@@ -9,6 +9,7 @@ class Task:
         self.__create_at = create_at
         self.__update = update 
 
+
     @classmethod   
     def create (cls,title:str , description:str):
         return Task(
@@ -18,7 +19,17 @@ class Task:
             create_at=datetime.now(),
             update= None)
     
-    
+    def update(self, title:str = None ,description:str = None):
+        if title is not None:
+            self.__title = title
+        if description is not None:
+            self.__description = description
+
+    def start(self):
+        self.__status = "starting"
+        
+    def finish(self):
+        self.__status = "finishing"
     
     def __repr__(self):
         return f"(title: {self.__title}, description: {self.__description}, status: {self.__status}, create_at: {self.__create_at}, update: {self.__update})"
--- main.py
+++ main.py
@@ -2,4 +2,9 @@
 from entities.task import Task
 
 task = Task.create(title="test oto", description= "description")
+task.start()
+print(task)
+
+task.update(description="nouvelle description")
+task.finish()
 print(task)
